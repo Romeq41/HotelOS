57fb41a2470f29a2200dd21393ef5e07fcc72c4a
romeq41 <jakubromanowski41@gmail.com>
2025-03-18 16:19:37 +0100

    initial commit

---
+++ HotelOS-backend/src/main/java/com/hotelos/hotelosbackend/HotelOsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotelos.hotelosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelOsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelOsBackendApplication.class, args);
+	}
+
+}
---
+++ HotelOS-backend/src/test/java/com/hotelos/hotelosbackend/HotelOsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelos.hotelosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelOsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
